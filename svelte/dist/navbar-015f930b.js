5fa1301763d41ea1b7b6de23e945005938e36ea5
Albert Gasset <albertgasset@fsfe.org>
2023-06-19 08:55:53 +0200

    Update production Javascript and CSS

--- svelte/dist/navbar-015f930b.js
+++
@@ -1,1 +0,0 @@
-import{a9 as t}from"./global-ce70547b.js";async function i(){const n=document.getElementById("local-mail-navbar");if(n){n.innerHTML="";const a=window.local_mail_navbar_data||{};new t({target:n,props:{settings:a.settings,strings:a.strings,menu:a.menu}})}}i();
