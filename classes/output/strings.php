59da9507d2c8827d3fc8acce77387748bdfb98df
Albert Gasset <albertgasset@fsfe.org>
2023-09-19 10:05:46 +0200

    Fix bundled strings overriden by AMOS packages when string cache is enabled

--- classes/output/strings.php
+++ classes/output/strings.php
@@ -26,7 +26,7 @@ class strings {
      * @return string The localized string.
      */
     public static function get(string $id, mixed $param = null): string {
-        return self::manager()->get_string($id, 'local_mail', $param);
+        return self::manager(current_language())->get_string($id, 'local_mail', $param);
     }
 
     /**
@@ -35,7 +35,9 @@ class strings {
      * @return string[] All localized strings.
      */
     public static function get_all(): array {
-        return self::manager()->load_component_strings('local_mail', current_language());
+        $lang = current_language();
+
+        return self::manager($lang)->load_component_strings('local_mail', $lang);
     }
 
     /**
@@ -44,7 +46,7 @@ class strings {
      * @return string[]
      */
     public static function get_ids(): array {
-        return array_keys(self::manager()->load_component_strings('local_mail', 'en'));
+        return array_keys(self::manager('en')->load_component_strings('local_mail', 'en'));
     }
 
     /**
@@ -60,58 +62,64 @@ class strings {
     /**
      * String manager that ignores local_mail language packages from AMOS for Catalan and Spanish.
      *
+     * @param string $lang Language.
      * @return \core_string_manager
      */
-    private static function manager(): \core_string_manager {
+    private static function manager(string $lang): \core_string_manager {
         global $CFG;
 
         static $instance = null;
 
-        if ($instance == null || PHPUNIT_TEST) {
-            $instance = new class($CFG->langotherroot, $CFG->langlocalroot, []) extends \core_string_manager_standard {
-
-                public function load_component_strings($component, $lang, $disablecache = false, $disablelocal = false) {
-                    global $CFG;
-
-                    if ($component == 'local_mail' && ($lang == 'ca' || $lang == 'es')) {
-                        // Ignore language packages from AMOS for Catalan and Spanish.
-
-                        $cachekey = $lang . '_' . $component . '_' . $this->get_key_suffix();
-                        $cachedstring = $this->cache->get($cachekey);
-                        if (!$disablecache && !$disablelocal && $cachedstring !== false) {
-                            return $cachedstring;
-                        }
-
-                        $string = [];
-
-                        // First load english pack.
-                        include("$CFG->dirroot/local/mail/lang/en/local_mail.php");
-
-                        // And then corresponding local english if present.
-                        if (!$disablelocal && file_exists("$CFG->langlocalroot/en_local/local_mail.php")) {
-                            include("$CFG->langlocalroot/en_local/local_mail.php");
-                        }
-
-                        // Legacy location - used by contrib only.
-                        include("$CFG->dirroot/local/mail/lang/$lang/local_mail.php");
-
-                        // Local customisations.
-                        if (!$disablelocal && file_exists("$CFG->langlocalroot/{$lang}_local/local_mail.php")) {
-                            include("$CFG->langlocalroot/{$lang}_local/local_mail.php");
-                        }
-
-                        // Save strings to cache.
-                        if (!$disablelocal && $cachedstring === false) {
-                            $this->cache->set($cachekey, $string);
-                        }
-
-                        return $string;
-                    }
-
-                    return parent::load_component_strings($component, $lang, $disablecache, $disablelocal);
-                }
-            };
+        if ($lang != 'ca' && $lang != 'es') {
+            return get_string_manager();
         }
+
+        if ($instance != null && !PHPUNIT_TEST) {
+            return $instance;
+        }
+
+        $instance = new class($CFG->langotherroot, $CFG->langlocalroot, []) extends \core_string_manager_standard {
+
+            public function load_component_strings($component, $lang, $disablecache = false, $disablelocal = false) {
+                global $CFG;
+
+                if ($component != 'local_mail' || ($lang != 'ca' && $lang != 'es')) {
+                    throw new \coding_exception('Internal local_mail error: unexpected use of the string manager');
+                }
+
+                // Use a different cache key than the standard string manager.
+                $cachekey = $lang . '_' . $component . '_' . $this->get_key_suffix() . '_bundled';
+                $cachedstring = $this->cache->get($cachekey);
+                if (!$disablecache && !$disablelocal && $cachedstring !== false) {
+                    return $cachedstring;
+                }
+
+                $string = [];
+
+                // First load english pack.
+                include("$CFG->dirroot/local/mail/lang/en/local_mail.php");
+
+                // And then corresponding local english if present.
+                if (!$disablelocal && file_exists("$CFG->langlocalroot/en_local/local_mail.php")) {
+                    include("$CFG->langlocalroot/en_local/local_mail.php");
+                }
+
+                // Legacy location - used by contrib only.
+                include("$CFG->dirroot/local/mail/lang/$lang/local_mail.php");
+
+                // Local customisations.
+                if (!$disablelocal && file_exists("$CFG->langlocalroot/{$lang}_local/local_mail.php")) {
+                    include("$CFG->langlocalroot/{$lang}_local/local_mail.php");
+                }
+
+                // Save strings to cache.
+                if (!$disablelocal && $cachedstring === false) {
+                    $this->cache->set($cachekey, $string);
+                }
+
+                return $string;
+            }
+        };
 
         return $instance;
     }
