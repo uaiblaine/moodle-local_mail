038b31a9f09433be11bd6f24287725f2fead498c
Albert Gasset <albertgasset@fsfe.org>
2024-03-14 20:51:37 +0100

    Fix error modal hiding immediately after showing up

--- svelte/src/actions/blur.ts
+++ svelte/src/actions/blur.ts
@@ -1,26 +1,33 @@
 /*
  * SPDX-FileCopyrightText: 2023-2024 Proyecto UNIMOODLE <direccion.area.estrategia.digital@uva.es>
+ * SPDX-FileCopyrightText: 2024 Albert Gasset <albertgasset@fsfe.org>
  *
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
 export function blur(node: HTMLElement, handler: () => void) {
-    function listener(event: Event) {
-        if (event.target instanceof Element && !node.contains(event.target)) {
+    function handleFocusOut(event: FocusEvent) {
+        if (event.relatedTarget instanceof Node && !node.contains(event.relatedTarget)) {
             handler();
         }
     }
 
-    document.addEventListener('click', listener, { capture: true, passive: true });
-    document.addEventListener('keyup', listener, { capture: true, passive: true });
+    function handleMouseDown(event: Event) {
+        if (event.target instanceof Node && !node.contains(event.target)) {
+            handler();
+        }
+    }
+
+    document.addEventListener('mousedown', handleMouseDown, { capture: true, passive: true });
+    node.addEventListener('focusout', handleFocusOut);
 
     return {
         update(newHandler: () => void) {
             handler = newHandler;
         },
         destroy() {
-            document.removeEventListener('click', listener, { capture: true });
-            document.removeEventListener('keyup', listener, { capture: true });
+            document.removeEventListener('mousedown', handleMouseDown, { capture: true });
+            node.removeEventListener('focusout', handleFocusOut);
         },
     };
 }
